3f394c0e58dc55a9ddbbb3d174eface44906d0ea
Vojtech Cerny <vojtech.cerny@accenture.com>
2017-02-15 15:33:18 +0200

    extracted script for ldap

--- bootstrap/Workspace_Management/Generate_Workspace.groovy
+++ bootstrap/Workspace_Management/Generate_Workspace.groovy
@@ -61,39 +61,18 @@ generateWorkspaceJob.with {
                 systemGroovyScriptFile('${WORKSPACE}/workspaces/groovy/acl_viewer.groovy')
             }
         }
-		conditionalSteps
-		{
-			condition
-			{
-				stringsMatch('${ADOP_LDAP_ENABLED}', 'true', true)
-			}
-			runner('Fail')
-			steps {
-				shell('''
-					 # LDAP
-					 ${WORKSPACE}/common/ldap/generate_role.sh -r "admin" -n "${WORKSPACE_NAME}" -d "${DC}" -g "${OU_GROUPS}" -p "${OU_PEOPLE}" -u "${ADMIN_USERS}" -f "${OUTPUT_FILE}" -w "${WORKSPACE}"
-					 ${WORKSPACE}/common/ldap/generate_role.sh -r "developer" -n "${WORKSPACE_NAME}" -d "${DC}" -g "${OU_GROUPS}" -p "${OU_PEOPLE}" -u "${DEVELOPER_USERS}" -f "${OUTPUT_FILE}" -w "${WORKSPACE}"
-					 ${WORKSPACE}/common/ldap/generate_role.sh -r "viewer" -n "${WORKSPACE_NAME}" -d "${DC}" -g "${OU_GROUPS}" -p "${OU_PEOPLE}" -u "${VIEWER_USERS}" -f "${OUTPUT_FILE}" -w "${WORKSPACE}"
+        conditionalSteps
+        {
+            condition
+            {
+			    stringsMatch('${ADOP_LDAP_ENABLED}', 'true', true)
+            }
+            runner('Fail')
+            steps {
+			    systemGroovyScriptFile('${WORKSPACE}/workspaces/groovy/ldap_roles.groovy')
+            }
+        }
 
-					 set +e
-					 ${WORKSPACE}/common/ldap/load_ldif.sh -h ldap -u "${LDAP_ADMIN_USER}" -p "${LDAP_ADMIN_PASSWORD}" -b "${DC}" -f "${OUTPUT_FILE}"
-					 set -e
-
-					 ADMIN_USERS=$(echo ${ADMIN_USERS} | tr ',' ' ')
-					 DEVELOPER_USERS=$(echo ${DEVELOPER_USERS} | tr ',' ' ')
-					 VIEWER_USERS=$(echo ${VIEWER_USERS} | tr ',' ' ')
-
-					 # Gerrit
-					 for user in $ADMIN_USERS $DEVELOPER_USERS $VIEWER_USERS
-					 do
-							 username=$(echo ${user} | cut -d'@' -f1)
-							 ${WORKSPACE}/common/gerrit/create_user.sh -g http://gerrit:8080/gerrit -u "${username}" -p "${username}"
-					 done
-					 ''')
-			}
-		}
-		
-		
         dsl
         {
             external("workspaces/jobs/**/*.groovy")
